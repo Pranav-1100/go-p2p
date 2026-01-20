6e0749302ee305c49148e3a64634f7caa653858f
Pranav Aggarwal <pranavaggarwal1100@gmail.com>
2026-01-20 23:03:29 +0530

    fixes in health

--- health.go
+++ health.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func startHealthServer() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{
+		if err := json.NewEncoder(w).Encode(map[string]string{
 			"status": "UP",
-		})
+		}); err != nil {
+			fmt.Printf("Error encoding health response: %v\n", err)
+		}
 	})
-	http.ListenAndServe(":8081", nil)
+
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		fmt.Printf("Health server error: %v\n", err)
+	}
 }
